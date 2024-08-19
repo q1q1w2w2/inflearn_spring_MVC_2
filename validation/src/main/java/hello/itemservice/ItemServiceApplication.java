cc94ef7cef811602778ee666d50fff6d941e9331
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-08-19 18:46:38 +0900

    2024.08.19 validation
    검증 로직, BindingResult로 오류 메시지 view로 전송, 오류 시점에서의 데이터 유지

---
+++ validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,14 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+}
