cb5eb1bfaa545268ede5ec461ca6ede5c272bec6
q1q1w2w2 <106463879+q1q1w2w2@users.noreply.github.com>
2024-08-23 16:28:26 +0900

    2024.08.23 typeconverter
    Converter 사용, 포멧터(@NumberFormat, DateTimeFormat)

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,34 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); // 문자 타입 조회
+        Integer intValue = Integer.valueOf(data); // 타입 변경
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    // 일반적인 컨버터들은 기본적으로 제공되지만
+    // 직접 만든 객체에 대한 변환은 WebConfig 로 등록해줘야 함
+    @GetMapping("/ip-port")
+    public String helloTest(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
+        return "ok";
+    }
+}
